3b799428ff53cfc6f280ae89bf1bffd40e0004de
Anet Lello <anet.lello@gmail.com>
2025-11-02 22:19:15 +0200

    Remove .DS_Store files to resolve merge conflicts

--- hw2/dbt/models/staging/stg_DimCompany.sql
+++ hw2/dbt/models/staging/stg_DimCompany.sql
@@ -5,14 +5,6 @@
 ) }}
 
 SELECT
-<<<<<<< HEAD
-    abs(mod(cast(hash(company) as bigint), 1000000000)) as CompanyKey,  -- Primary Key - unfortunately we are not able to load it from source and thus need to generate from name. should not change whatever happens to company
-    company as CompanyName,  
-    headquarters as Headquarters,
-    industry as Industry,
-    Sector --TOBEDELETED IF not existing
-FROM {{ source('bronze', 'companies_raw') }};
-=======
     CAST(xxHash64(concat(company,headquarters)) AS UInt32) AS CompanyKey,  -- Primary Key
     CAST(company AS String) AS CompanyName,
     CAST(headquarters AS String) AS Headquarters,
@@ -20,4 +12,3 @@ FROM {{ source('bronze', 'companies_raw') }};
     CAST('sector' AS String) AS Sector,  -- optional, can drop later if not needed
     CAST(_ingested_at AS DateTime) AS _load_datetime
 FROM {{ source('bronze', 'companies_raw') }}
->>>>>>> dbt
