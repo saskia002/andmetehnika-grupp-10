ab6cba118cc69151538a2bccacd085b8f88e77d5
Anet Lello <anet.lello@gmail.com>
2025-11-28 00:09:45 +0200

    adding tables in limited_views as well

--- hw3/clickhouse/Privacy_and_Security/03_Create_limited_views.sql
+++ hw3/clickhouse/Privacy_and_Security/03_Create_limited_views.sql
@@ -1,7 +1,11 @@
 -- Create schema for limited views
 CREATE DATABASE IF NOT EXISTS gold_limited_views;
 
-CREATE OR REPLACE VIEW gold_limited_views.v_limited_DimCompany AS
+DROP TABLE IF EXISTS gold_limited_views.v_limited_DimCompany;
+CREATE TABLE gold_limited_views.v_limited_DimCompany
+ENGINE = MergeTree()
+ORDER BY CompanyKey
+AS
 SELECT
     CompanyKey,    
     concat(substring(CompanyName, 1, 3), '***') AS CompanyName_masked, -- Show first 3 chars + ***
@@ -27,7 +31,11 @@ FROM
     FROM gold.DimCompany
 ) t;
 
-CREATE OR REPLACE VIEW gold_limited_views.v_limited_DimDate AS
+DROP TABLE IF EXISTS gold_limited_views.v_limited_DimDate;
+CREATE TABLE gold_limited_views.v_limited_DimDate
+ENGINE = MergeTree()
+ORDER BY DateKey
+AS
 SELECT
     DateKey,
     TradingDate,
@@ -37,7 +45,11 @@ SELECT
     Quarter
 FROM gold.DimDate;
 
-CREATE OR REPLACE VIEW gold_limited_views.v_limited_DimTicker AS
+DROP TABLE IF EXISTS gold_limited_views.v_limited_DimTicker;
+CREATE TABLE gold_limited_views.v_limited_DimTicker
+ENGINE = MergeTree()
+ORDER BY TickerKey
+AS
 SELECT
     TickerKey,
     TickerSymbol,
@@ -49,7 +61,11 @@ SELECT
     ValidTo      
 FROM gold.DimTicker;
 
-CREATE OR REPLACE VIEW gold_limited_views.v_limited_FactFinancials AS
+DROP TABLE IF EXISTS gold_limited_views.v_limited_FactFinancials;
+CREATE TABLE gold_limited_views.v_limited_FactFinancials
+ENGINE = MergeTree()
+ORDER BY (CompanyKey, Year)
+AS
 SELECT
     CompanyKey,
     Year,
@@ -59,7 +75,11 @@ SELECT
     MarketValue
 FROM gold.FactFinancials;
 
-CREATE OR REPLACE VIEW gold_limited_views.v_limited_FactForbesRank AS
+DROP TABLE IF EXISTS gold_limited_views.v_limited_FactForbesRank;
+CREATE TABLE gold_limited_views.v_limited_FactForbesRank
+ENGINE = MergeTree()
+ORDER BY (CompanyKey, Year)
+AS
 SELECT
     ForbesRankKey,
     CompanyKey,
@@ -71,7 +91,11 @@ SELECT
     ) AS ForbesRank_range -- MASKED COLUMN as range
 FROM gold.FactForbesRank;
 
-CREATE OR REPLACE VIEW gold_limited_views.v_limited_FactStock AS
+DROP TABLE IF EXISTS gold_limited_views.v_limited_FactStock;
+CREATE TABLE gold_limited_views.v_limited_FactStock
+ENGINE = MergeTree()
+ORDER BY (DateKey, CompanyKey, TickerKey)
+AS
 SELECT
     DateKey,
     CompanyKey,
