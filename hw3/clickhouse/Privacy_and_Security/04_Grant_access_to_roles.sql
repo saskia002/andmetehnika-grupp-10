afb0bde2c31ff8de4a9ca7ef706876b02e20095c
Anet Lello <anet.lello@gmail.com>
2025-11-27 14:22:30 +0200

    small typo

--- hw3/clickhouse/Privacy_and_Security/04_Grant_access_to_roles.sql
+++ hw3/clickhouse/Privacy_and_Security/04_Grant_access_to_roles.sql
@@ -1,5 +1,5 @@
 -- Grant SELECT on full views to role _full
-GRANT USAGE ON DATABASE gold_lfull_views TO analyst_full;
+GRANT USAGE ON DATABASE gold_full_views TO analyst_full;
 GRANT SELECT ON gold_full_views.*  TO analyst_full;
 
 -- Grant SELECT on masked views to role _limited
