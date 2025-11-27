80b44a9462c3d7ca675c253e50d1cef58f01d4ec
Anet Lello <anet.lello@gmail.com>
2025-11-27 14:43:08 +0200

    testing on one view

--- hw3/clickhouse/Privacy_and_Security/04_Grant_access_to_roles.sql
+++ hw3/clickhouse/Privacy_and_Security/04_Grant_access_to_roles.sql
@@ -1,9 +1,9 @@
 -- Grant SELECT on full views to role _full
 GRANT USAGE ON DATABASE gold_full_views TO analyst_full;
-GRANT SELECT ON gold_full_views.v_*  TO analyst_full;
+GRANT SELECT ON gold_full_views.v_DimCompany  TO analyst_full;
 GRANT SHOW ON DATABASE gold TO analyst_full;
 
 -- Grant SELECT on masked views to role _limited
 GRANT USAGE ON DATABASE gold_limited_views TO analyst_limited;
-GRANT SELECT ON gold_limited_views.v_limited*  TO analyst_limited;
+GRANT SELECT ON gold_limited_views.*  TO analyst_limited;
 GRANT SHOW ON DATABASE gold TO analyst_limited;
